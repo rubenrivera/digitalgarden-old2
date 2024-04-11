4b3621560b0b4772c6ed108cd608f3b043339d22
rubenrivera <rr@rubenrivera.mx>
2024-04-11 14:34:15 -0600

    *

--- src/helpers/userSetup.js
+++ src/helpers/userSetup.js
@@ -1,10 +1,30 @@
+const { EleventyI18nPlugin, EleventyServerlessBundlerPlugin } = require("@11ty/eleventy");
+
+// Translations
 function userMarkdownSetup(md) {
   // The md parameter stands for the markdown-it instance used throughout the site generator.
   // Feel free to add any plugin you want here instead of /.eleventy.js
 }
+
 function userEleventySetup(eleventyConfig) {
   // The eleventyConfig parameter stands for the the config instantiated in /.eleventy.js.
   // Feel free to add any plugin you want here instead of /.eleventy.js
+
+  /**
+   * Internationalization
+   */
+  eleventyConfig.addPlugin(EleventyI18nPlugin, {
+    defaultLanguage: 'es',
+    errorMode: 'allow-fallback'
+  });
+  /**
+   * Serverless
+   */
+  eleventyConfig.addPlugin(EleventyServerlessBundlerPlugin, {
+    name: "authorization",
+    functionsDir: "./netlify/functions/",
+  });
 }
+
 exports.userMarkdownSetup = userMarkdownSetup;
 exports.userEleventySetup = userEleventySetup;
---
+++ src/site/_data/languages.js
@@ -0,0 +1,8 @@
+module.exports = {
+    en : {
+        availableText: 'This page is also available in:'
+    },
+    es: {
+        availableText: 'Esta página también está disponible en:'
+    }
+}
