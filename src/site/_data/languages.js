497af37b0dc6565a5a6d21b6331f98b86dad4ed4
rubenrivera <rr@rubenrivera.mx>
2024-04-12 06:59:03 -0600

    *

--- src/site/_data/languages.js
+++ src/site/_data/languages.js
@@ -1,8 +1,22 @@
 module.exports = {
     en : {
-        availableText: 'This page is also available in:'
+        availableText: 'This page is also available in:',
+        userData: 'User Data',
+        login: 'Login',
+        logout: 'Logout',
+        thisPageIsSecure: 'this page is secure.',
+        nothingHere: '',
+        thereIsNothingHere: 'There is nothing here',
+        ifYouHereFrom: 'If you got here from a link, this note is probably not made public'
     },
     es: {
-        availableText: 'Esta página también está disponible en:'
+        availableText: 'Esta página también está disponible en:',
+        userData: "Información de Usuario",
+        login: 'Acceder',
+        logout: 'Salir',
+        thisPageIsSecure: 'esta página es segura.',
+        nothingHere: '',
+        thereIsNothingHere: 'No hay nada aquí',
+        ifYouHereFrom: 'Si llegaste aquí siguiendo un enlace, es probable que la nota no haya sido publicada'
     }
 }
