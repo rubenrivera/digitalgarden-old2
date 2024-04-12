2c228f28214fbfa1cda25e216b70e1b99eacd83e
Rubén Rivera <rr@rubenrivera.mx>
2024-04-12 10:55:29 -0600

    *

--- src/site/_data/languages.js
+++ src/site/_data/languages.js
@@ -2,21 +2,25 @@ module.exports = {
     en : {
         availableText: 'This page is also available in:',
         userData: 'User Data',
+        loginTitle: 'Login',
         login: 'Login',
         logout: 'Logout',
         thisPageIsSecure: 'this page is secure.',
         nothingHere: '',
         thereIsNothingHere: 'There is nothing here',
-        ifYouHereFrom: 'If you got here from a link, this note is probably not made public'
+        ifYouHereFrom: 'If you got here from a link, this note is probably not made public',
+        youAreAlready: 'You are already logged in!'
     },
     es: {
         availableText: 'Esta página también está disponible en:',
         userData: "Información de Usuario",
+        loginTitle: 'Acceder',
         login: 'Acceder',
         logout: 'Salir',
         thisPageIsSecure: 'esta página es segura.',
         nothingHere: '',
         thereIsNothingHere: 'No hay nada aquí',
-        ifYouHereFrom: 'Si llegaste aquí siguiendo un enlace, es probable que la nota no haya sido publicada'
+        ifYouHereFrom: 'Si llegaste aquí siguiendo un enlace, es probable que la nota no haya sido publicada',
+        youAreAlready: '¡Ya haz accedido!'
     }
 }
